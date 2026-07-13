92686f1ef2094a070f65a8be8bf9627262e727d9
rahul kahar 88 <rahulkahar88588@gmail.com>
2026-07-13 17:14:36 +0530

    Update

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -99,7 +99,7 @@ return (
       Shop By Category
     </h2>
 
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-4">
 
       <div
         onClick={() => setSelectedCategory("")}
